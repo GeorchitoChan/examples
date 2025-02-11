28400779f3893178bd12f92e68ec779058f9721f
Jorge Chan <jorgeluis.chanchan@gmail.com>
2025-02-11 12:47:08 -0600

    Permite inicializar el repositorio con la estructura del proyecto.

---
+++ src/main/java/com/mycompany/examples/Examples.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examples;
+
+/**
+ *
+ * @author GeorchitoChan
+ */
+public class Examples {
+
+    public static void main(String[] args) {
+        
+    }
+}
